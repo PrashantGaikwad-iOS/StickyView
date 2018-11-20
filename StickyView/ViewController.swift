7a939d5b34b10d1b675a88e1b5bf226bed618c32
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-20 15:05:38 +0530

    Header and Sticky view added on tableview

--- StickyView/ViewController.swift
+++ StickyView/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.estimatedSectionHeaderHeight = 100
+        let hView = UIView(frame: CGRect(x: 10, y: 0, width: tableView.frame.width, height: 50))
+        hView.backgroundColor = .red
+        tableView.tableHeaderView = hView
     }
-
-
 }
 
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        
+        let segmentedControl = UISegmentedControl(frame: CGRect(x: 10, y: 15, width: tableView.frame.width-20, height: 30))
+        segmentedControl.tintColor = .white
+        segmentedControl.insertSegment(withTitle: "One", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "Two", at: 1, animated: false)
+        segmentedControl.insertSegment(withTitle: "Three", at: 2, animated: false)
+        
+        header.addSubview(segmentedControl)
+        header.backgroundColor = .blue
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+}
